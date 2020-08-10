f67120ee3cc438dac722963e469a76be3cf3dbd0
namnguyen191 <hoangnamnguyen191@gmail.com>
2020-08-10 14:53:32 -0400

    Refractor passport to use await/async syntax

--- server-with-es6-import/services/passport.js
+++ server-with-es6-import/services/passport.js
@@ -26,18 +26,15 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id }).then((existingUser) => {
-                if (existingUser) {
-                    // We already have a user
-                    done(null, existingUser);
-                } else {
-                    // New user
-                    new User({ googleId: profile.id }).save().then((user) => {
-                        done(null, user);
-                    });
-                }
-            });
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id });
+            if (existingUser) {
+                // We already have a user
+                return done(null, existingUser);
+            }
+            // New user
+            const newUser = new User({ googleId: profile.id }).save();
+            done(null, newUser);
         }
     )
 );
